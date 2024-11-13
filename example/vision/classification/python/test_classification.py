c2d58cab86f48d7ec45f0ec37b73415f5af556b1
Zheng-Bicheng <zheng_bicheng@outlook.com>
2024-11-13 02:52:37 +0000

    !17 优化分类模型 * 更新使用 RKNN Toolkit2 转换模型 * 更新 FP32 分类模型导出

--- example/vision/classification/python/test_classification.py
+++ example/vision/classification/python/test_classification.py
@@ -1,21 +1,10 @@
-from lockzhiner_vision_module.cv2 import VideoCapture
+from lockzhiner_vision_module.cv2 import VideoCapture, imread
 from lockzhiner_vision_module.vision import PaddleClas
 import time
 import sys
 
-if __name__ == "__main__":
-    args = sys.argv
-    if len(args) != 2:
-        print(
-            "Need model path. Example: python test_classification.py LZ-MobileNetV3.rknn"
-        )
-        exit(1)
 
-    model = PaddleClas()
-    if model.initialize(args[1]) is False:
-        print("Failed to initialize PaddleClas")
-        exit(1)
-
+def predict_video(cls_model):
     video_capture = VideoCapture()
     if video_capture.open(0) is False:
         print("Failed to open capture")
@@ -30,9 +19,34 @@ if __name__ == "__main__":
                 continue
 
             start_time = time.time()
-            result = model.predict(mat)
+            result = cls_model.predict(mat)
             end_time = time.time()
             total_time_ms += end_time - start_time
             read_index += 1
             print(result.label_id, result.score)
         print(f"FPS is {1.0 / (total_time_ms/read_index)}")
+
+
+def predict_image(cls_model, image_path):
+    image = imread(image_path)
+    result = cls_model.predict(image)
+    print(result.label_id, result.score)
+    
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) < 2:
+        print(
+            "Need model path. Example: python test_classification.py LZ-MobileNetV3.rknn"
+        )
+        exit(1)
+
+    model = PaddleClas()
+    if model.initialize(args[1]) is False:
+        print("Failed to initialize PaddleClas")
+        exit(1)
+
+    if len(args) == 2:
+        predict_video(model)
+    elif len(args) == 3:
+        predict_image(model, args[2])
